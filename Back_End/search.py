58042034f35453f0105cb4970d19b28108c6a03d
Rel1sh1 <330526602@qq.com>
2024-06-13 16:39:17 +0800

    主界面的连接以及第二次设计

--- Back_End/search.py
+++ Back_End/search.py
@@ -11,13 +11,20 @@ def search_historyData(request):
     ctx = {}
     if request.POST:
         ctx['rlt'] = request.POST['q']
+        date_str = str(ctx['rlt'])
+        data_str = str(ctx['rlt'])
+
+        # 使用切片替换字符串中的短横线
+        data_str = data_str.replace('-', '/')
+        data_str = "/".join(part.lstrip('0') for part in data_str.split('/'))
+        ds = data_str + ' '
         # 指定 CSV 文件路径
         csv_file_path = 'cleaned_data.csv'
         # 调用函数提取数据
         data = views.extract_data(csv_file_path)
         # 筛选指定日期的信息
         # 第一行不参与判断
-        data = [row for i, row in enumerate(data) if i == 0 or str(ctx['rlt']) in str(row[0])]
+        data = [row for i, row in enumerate(data) if i == 0 or ds in str(row[0])]
         for item in data:
             for _ in range(27):
                 item.pop(1)
@@ -29,6 +36,7 @@ def search_historyData(request):
         return render(request, "historyDatapage.html", {"rlt": data_json})
     else:
         return render(request, "historyDatapage.html", {"rlt": ctx})
+
 
 #预测数据
 def search_ForecastData(request):
