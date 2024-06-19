653b286c2e16ac6790f92089229d756109490f15
LiSihan <1239504831@qq.com>
2024-06-19 10:24:56 +0800

    final update

---
+++ data_process/analyse.py
@@ -0,0 +1,30 @@
+# 分析每天出现雾霾最多的时段
+import pandas as pd
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+from datetime import datetime as dt
+
+# 读取CSV文件
+data = pd.read_csv('../output1.csv')
+
+# 初始化列表
+hour_counts = [0] * 24
+
+data['time'] = pd.to_datetime(data['timestamp_local'])
+data['date'] = data['time'].dt.date
+data['hour'] = data['time'].dt.hour
+
+hourly_aqi = data.groupby(['date', 'hour'])['aqi'].mean().reset_index()
+
+# 找出每天AQI值最高的4个小时
+for date, group in hourly_aqi.groupby('date'):
+    top_hours = group.nlargest(4, 'aqi')['hour']
+    for hour in top_hours:
+        hour_counts[hour] += 1
+
+# 使用pyecharts绘制图像
+bar = Bar()
+bar.add_xaxis([str(i) + ":00" for i in range(24)])
+bar.add_yaxis('次数', hour_counts)
+bar.set_global_opts(title_opts=opts.TitleOpts(title='每天AQI值最高的时段'))
+bar.render('hourly_aqi.html')
--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-now = datetime.now()
-current_hour_timestamp = int(now.replace(minute=0, second=0, microsecond=0).timestamp())
-pre_24_hour = now - timedelta(hours=24)
-pre_24_hour = pre_24_hour.replace(minute=0, second=0, microsecond=0)
-pre_24_hour = int(pre_24_hour.timestamp())
-print(current_hour_timestamp)
-print(pre_24_hour)
